921cca87a115501490306ab30b7728ea778eee37
hzlloni <jhw030329@gmail.com>
2023-07-19 15:54:47 +0900

    add

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:hellonong/util/color_schemes.g.dart';
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: [
+          Icon(
+            Icons.list_alt_rounded,
+            size: 40,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.person_outline,
+            size: 40,
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 26, 0, 0),
+          child: Column(
+            children: [
+              Container(
+                height: 150,
+                width: 178,
+                child: Image.asset(
+                  "assets/images/Frame 2608516.png",
+                  width: 72,
+                  height: 72,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.7),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0,3)
+                      )
+                    ]
+                ),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: Text("병원")),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                height: 150,
+                width: 178,
+                child: Image.asset(
+                  "assets/images/Pharmacy.png",
+                  width: 72,
+                  height: 72,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.7),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0,3)
+                      )
+                    ]
+                ),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: Text("약국")),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                height: 150,
+                width: 178,
+                child: Image.asset(
+                  "assets/images/Zoom in.png",
+                  width: 72,
+                  height: 72,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.7),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0,3)
+                      )
+
+                    ]
+                ),
+              )
+
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,57 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hellonong/util/color_schemes.g.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
+import 'app.dart';
+import 'home.dart';
+
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: lightColorScheme,
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: ' '),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(widget.title),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Theme.of(context).colorScheme.primary, // 배경색 설정
-        child: Center(
-          // Add your widgets here to fill the screen
-        ),
-      ),
-    );
-  }
-}
